9d638b6efed3296f034ed0fdc5cadb5d3c9ad672
Jad Chaar <jad.chaar@gmail.com>
2018-12-21 20:58:21 -0500

    Added cleanup to publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,9 @@
+# Cleanup old packages
+rm -rf sec_edgar_downloader.egg-info
+rm -rf dist
+rm -rf build
+
+# Upload package to PyPI
 # https://packaging.python.org/tutorials/packaging-projects/
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
