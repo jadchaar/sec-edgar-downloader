f888f8f60903f2230af6f1aa70a25f2f6f81897a
Jad Chaar <jadchaar@users.noreply.github.com>
2019-06-17 17:41:08 -0400

    Added full text unit test and changed fixture to use pathlib (#16)
    * Added test to ensure that filings downloads match exactly with manually downloaded filing; changed fixtures to use tmp_path rather than tmpdir
    
    * Improved git commit hooks
    
    * Removed meta tags

--- tests/test_before_date.py
+++ tests/test_before_date.py
@@ -1,9 +1,9 @@
-from .testing_utils import verify_directory_structure
+from ._testing_utils import verify_directory_structure
 
 
 def test_before_date_filing_retrieval(downloader, apple_filing_metadata_pre_2007):
     # 12/13/1994 is the date of Apple's first filing
-    dl, download_location = downloader
+    dl, download_folder_base = downloader
 
     num_downloaded = dl.get_10k_filings(
         apple_filing_metadata_pre_2007["symbol"], 100, "19941213"
@@ -16,7 +16,7 @@ def test_before_date_filing_retrieval(downloader, apple_filing_metadata_pre_2007
     assert num_downloaded == 0
 
     verify_directory_structure(
-        download_location,
+        download_folder_base,
         ["10-K"],
         1,
         **apple_filing_metadata_pre_2007,
