703fa5e25b3613cd266b02a695a5eadcf911fa17
Jad Chaar <jad.chaar@gmail.com>
2019-06-23 19:09:32 -0400

    Updated version and added master_doc to sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,8 @@ extensions = ["sphinx.ext.autodoc"]
 
 autodoc_member_order = "bysource"
 
+master_doc = "index"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
