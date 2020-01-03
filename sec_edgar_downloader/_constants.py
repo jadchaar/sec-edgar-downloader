57cff24a68bbe82c8a1cec810ec69c19c8a61575
Jad Chaar <jad.chaar@gmail.com>
2020-01-03 15:39:42 -0500

    Completed test suite rewrite. Deleted remaining code from v2.

--- sec_edgar_downloader/_constants.py
+++ sec_edgar_downloader/_constants.py
@@ -2,7 +2,7 @@ SEC_EDGAR_BASE_URL = "https://www.sec.gov/cgi-bin/browse-edgar?"
 SUPPORTED_FILINGS = {
     "8-K",
     "10-K",
-    "10-KSB",
+    "10KSB",
     "10-Q",
     "13F-NT",
     "13F-HR",
--- tests/test_filing_url_retrieval.py
+++ tests/test_filing_url_retrieval.py
@@ -60,3 +60,68 @@ def test_filing_url_retrieval():
     )
     # there are 176 AAPL 8-K filings before 20191115
     assert len(filings_to_download) == 176
+
+
+def test_all_filings_apple():
+    # AAPL files 8-K, 10-K, 10-Q, SC 13G, SD
+    ticker = "AAPL"
+    filing_types = ["8-K", "10-K", "10-Q", "SC 13G", "SD"]
+    num_filings_to_download = 1
+    after_date = None
+    before_date = None
+    include_amends = False
+
+    for filing_type in filing_types:
+        filings_to_download = get_filing_urls_to_download(
+            filing_type,
+            ticker,
+            num_filings_to_download,
+            after_date,
+            before_date,
+            include_amends,
+        )
+        assert len(filings_to_download) == 1
+
+
+def test_all_filings_vanguard():
+    # Vanguard files 13F-NT, 13F-HR
+    ticker = "0000102909"
+    filing_types = [
+        "13F-NT",
+        "13F-HR",
+    ]
+    num_filings_to_download = 1
+    after_date = None
+    before_date = None
+    include_amends = False
+
+    for filing_type in filing_types:
+        filings_to_download = get_filing_urls_to_download(
+            filing_type,
+            ticker,
+            num_filings_to_download,
+            after_date,
+            before_date,
+            include_amends,
+        )
+        assert len(filings_to_download) == 1
+
+
+def test_all_filings_ubiquitech():
+    # Ubiquitech files 10KSB
+    ticker = "0001411460"
+    filing_type = "10KSB"
+    num_filings_to_download = 1
+    after_date = None
+    before_date = None
+    include_amends = False
+
+    filings_to_download = get_filing_urls_to_download(
+        filing_type,
+        ticker,
+        num_filings_to_download,
+        after_date,
+        before_date,
+        include_amends,
+    )
+    assert len(filings_to_download) == 1
